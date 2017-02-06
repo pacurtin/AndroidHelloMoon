d50c9bfde9a5aad5e180fee4b713b7808a74b9ed
Paddy <pacurtin@tcd.ie>
2017-02-06 12:15:44 +0000

    Chapter 15 finished

--- app/src/main/java/com/example/padraigcurtin/hellomoon/HelloMoonActivity.java
+++ app/src/main/java/com/example/padraigcurtin/hellomoon/HelloMoonActivity.java
@@ -2,8 +2,9 @@ package com.example.padraigcurtin.hellomoon;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class HelloMoonActivity extends FragmentActivity {
+public class HelloMoonActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
